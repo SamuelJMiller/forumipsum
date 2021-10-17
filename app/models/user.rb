4a14bd8e9ddd713c024e6404141ad7c62b72df0f
Sam Miller <sam@itsecureadmin.com>
2021-10-17 08:31:31 -0700

    add username uniqueness to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
   # All devise modules are enabled in the database migration file
+
+  # Make sure username is unique and handle name conflicts gracefully
+  validates_uniqueness_of :username
 end
