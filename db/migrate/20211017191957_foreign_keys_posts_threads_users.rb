4972f2d37c33ed05cf91df75100fafa8ee2da95b
Sam Miller <sam@itsecureadmin.com>
2021-10-17 21:36:42 -0700

    make db migration reversible

--- db/migrate/20211017191957_foreign_keys_posts_threads_users.rb
+++ db/migrate/20211017191957_foreign_keys_posts_threads_users.rb
@@ -1,7 +1,7 @@
 class ForeignKeysPostsThreadsUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :posts, :forumthread_id, :integer
-    remove_column :posts, :thread_id
+    remove_column :posts, :thread_id, :integer
     
     add_foreign_key :posts, :users, foreign_key: true
     add_foreign_key :posts, :forumthreads, foreign_key: true
