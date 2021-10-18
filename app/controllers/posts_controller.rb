b3287f656de3c733647cc6da366a90c35ec45574
Sam Miller <sam@itsecureadmin.com>
2021-10-18 16:40:53 -0700

    only show each thread's posts on post index pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,8 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @forumthread = Forumthread.find(params[:forumthread_id])
+    @posts = @forumthread.posts.all
   end
 
   # GET /posts/1 or /posts/1.json
