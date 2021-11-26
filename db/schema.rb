25ca597c6b5894eb5fe518819485c2aa1c05035a
Sam Miller <sam@itsecureadmin.com>
2021-11-26 10:50:16 -0800

    add category table again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_26_173446) do
+ActiveRecord::Schema.define(version: 2021_11_26_184916) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "forumthreads", force: :cascade do |t|
     t.string "title"
