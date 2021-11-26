f9e2e24eb679f6374dccea013bb8516c4066ab70
Sam Miller <sam@itsecureadmin.com>
2021-11-26 14:00:41 -0800

    begin category show script

--- app/javascript/custom/categories.js
+++ app/javascript/custom/categories.js
@@ -1,1 +1,10 @@
 let dropped = false;
+let tables = document.getElementsByClassName("border-table");
+
+document.getElementsByClassName("category-show-button")[0].onclick(onclick());
+
+function onclick() {
+    for ( let i = 0; i < tables.length; ++i ) {
+        tables[i].style.visibility = "hidden";
+    }
+}
