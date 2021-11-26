f3e4660d877bf69bbeadc4689b0cbeb55db2c1e5
Sam Miller <sam@itsecureadmin.com>
2021-11-26 10:54:45 -0800

    add Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_26_184916) do
+ActiveRecord::Schema.define(version: 2021_11_26_185349) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "forumthreads", force: :cascade do |t|
