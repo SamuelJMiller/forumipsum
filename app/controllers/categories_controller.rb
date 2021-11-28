da3cbde9c852a4813546592f10c0499537b97bb5
Sam Miller <sam@itsecureadmin.com>
2021-11-28 15:45:21 -0800

    draft new category form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,15 @@
 class CategoriesController < ApplicationController
     def new
+        @category = Category.create
+    end
+
+    def create
+        @category = Category.create(category_params)
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
     end
 end
