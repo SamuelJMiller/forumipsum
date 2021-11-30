a8fd2f70071290ebd2802bf7550196f48afab8b1
Sam Miller <sam@itsecureadmin.com>
2021-11-30 09:10:26 -0800

    added functional category rollup/rolldown javascript

--- app/javascript/custom/categories.js
+++ app/javascript/custom/categories.js
@@ -1,3 +1,16 @@
-let table = document.getElementById("table-cat-1");
+window.onload = function() {
+    let headers = document.getElementsByClassName('category-name-span');
+    let tables = document.getElementsByClassName('border-table');
 
-console.log(table);
+    if (headers.length > 0) {
+        for ( let i = 0; i < headers.length; ++i ) {
+            headers[i].addEventListener('click', function() {
+                if (tables[i].className === 'border-table') {
+                    tables[i].className += ' hide';
+                } else {
+                    tables[i].className = 'border-table';
+                }
+            });
+        }
+    }
+}
