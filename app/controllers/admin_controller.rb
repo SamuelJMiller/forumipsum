93760fb2e5024942ac006cf69eb8bc4f06998741
Sam Miller <sam@itsecureadmin.com>
2022-01-17 15:55:11 -0800

    fix database queries to use single equals signs instead of double equals signs for mysql

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ApplicationController
     def index
-        @forumthreads = Forumthread.where('report_weight > ?', 0).where('is_banned == ?', false).order("report_weight DESC")
-        @posts = Post.where('report_weight > ?', 0).where('is_banned == ?', false).order("report_weight DESC")
+        @forumthreads = Forumthread.where('report_weight > ?', 0).where('is_banned = ?', false).order("report_weight DESC")
+        @posts = Post.where('report_weight > ?', 0).where('is_banned = ?', false).order("report_weight DESC")
     end
 end
